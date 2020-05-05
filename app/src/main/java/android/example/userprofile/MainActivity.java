90d5f31af05234a3aff5718da1c879deac55b0c2
Geoffrey Bosire Jr <gbosirejr@gmail.com>
2020-05-04 20:26:33 -0800

    added imageview and changed layout to relative

--- app/src/main/java/android/example/userprofile/MainActivity.java
+++ app/src/main/java/android/example/userprofile/MainActivity.java
@@ -3,6 +3,8 @@ package android.example.userprofile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        ImageView profilePic = findViewById(R.id.profile_picture);
+        profilePic.setImageResource(R.drawable.profilepic);
+
+        TextView name = findViewById(R.id.name);
+        TextView birthday = findViewById(R.id.birthday);
+        TextView country = findViewById(R.id.country);
+
+        name.setText("Geoffrey Boss");
+        name.setTextSize(32);
+        birthday.setText("August 19");
+        birthday.setTextSize(32);
+        country.setText("USA/Kenya");
+        country.setTextSize(32);
     }
 }
